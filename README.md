2d1ba81cd9c78fadd1ac54f99095b9d29729a01d
ashwin-builds <ashwin.ganapathy78@gmail.com>
2024-07-06 14:20:07 +0530

    Adding all files

---
+++ README.md
@@ -0,0 +1,4 @@
+# News Updater
+
+### Summary
+This program scrapes news websites and returns the titles of the news within the terminal. It is too much work to load news websites and go through the information in an unstructured way. This program allows you to understand current world events quickly in the morning.
---
+++ code1.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+
+#WEBSITES
+#WORLD NEWS 1: https://www.thehindu.com/news/international/
+#india news 2: https://www.thehindu.com/news/national/
+
+def get_th_titles(link):
+    titles = []
+    response = requests.get(link)
+    soup = BeautifulSoup(response.content, "html.parser")
+    h3 = soup.find_all("h3")
+    for tag in h3:
+        class_name = tag.get("class")
+        if class_name:
+            if("title" in class_name):
+                titles.append(tag.text.replace("\n", ""))
+    return titles
+
+WORLD_NEWS_LINK = 'https://www.thehindu.com/news/international/'
+INDIA_NEWS_LINK = 'https://www.thehindu.com/news/national/'
+
+world_news_titles = get_th_titles(WORLD_NEWS_LINK)
+india_news_titles = get_th_titles(INDIA_NEWS_LINK)
+
+#outputting into terminal
+print("-" * 20)
+print('\n')
+print("---WORLD NEWS---\n\n")
+for title in world_news_titles:
+    print(title + '\n')
+print("\n\n\n---INDIA NEWS---\n\n\n")
+for title in india_news_titles:
+    print(title + "\n")
+print("\n")
+print("-" * 20)
